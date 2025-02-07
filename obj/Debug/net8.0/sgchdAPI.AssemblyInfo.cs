3b7cdd60f048aca6e8487032ee04a3fe1931b33c
jamerson-mt <jjamersonmt@gmail.com>
2025-02-07 08:28:09 -0300

    merge need

--- obj/Debug/net8.0/sgchdAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/sgchdAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sgchdAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09a2c2468497bb394ceef1621b3af98279b7f75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f63cabc0f95a4307956993598b6024bcfc88cb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("sgchdAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sgchdAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
